98eb5931823dab07b7bb0ef01b815619790dcc06
Justin Plein <2099449@talnet.nl>
2020-03-05 15:41:34 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,15 +1,3 @@
 SELECT * FROM planeten; 
 
-DROP TABLE animals;
-
-ALTER TABLE planeten DROP COLUMN bezoek_datum;
-
-
-CREATE TABLE planeten (
-     naam VARCHAR (30) NOT NULL,
-     diameter INT,
-     afstand_tot_de_zon float,
-     massa float,
-     bezoek_datum date NOT NULL,
-     id MEDIUMINT NOT NULL AUTO_INCREMENT primary key
-);
+UPDATE `sterrenstelsel`.`planeten` set `naam` = 'Teenalp' WHERE (id = '10');
